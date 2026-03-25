bf6d21c01c773b2919040ba69090553a3330ce5a
Gabriel Ventorim <gabriel.ventorims@gmail.com>
2026-03-25 00:07:20 -0300

    define strategy

--- src/main.py
+++ src/main.py
@@ -1,5 +1,26 @@
 def main():
-    print("test")
+
+    # == RUM DEFINITION == 
+
+    # get company names (file or database -> not sure each one will be better)
+
+    # get the historical bias based on the code [internal] -> database
+
+    # get infos + news about the companies using the web scraper [external] -> do the news (prob .json) need to be save? (i dont think so)
+
+    # IA work to resume and give a rate using the external infos -> local vars
+
+    # combine [ (internal + external) <-- result ] -> local vars
+
+    # apply result to generate rating -> database
+    
+    # use the result + the previous bias to calculate a next bias for the next runs -> database
+    
+    # RESULT:
+    # id_company, rating_position, text_about_company, date -> database
+    # table MUST have date as an index!!!!!!
+
+    print("Hello World!")
 
 if __name__ == "__main__":
     main()
